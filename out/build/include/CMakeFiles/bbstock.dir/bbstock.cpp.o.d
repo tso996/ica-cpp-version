bd0d4a11529f352420f7815e73de3e854afff9f4
Joe <51638822+tso996@users.noreply.github.com>
2022-06-05 01:05:35 +0100

    real clean up

--- out/build/include/CMakeFiles/bbstock.dir/bbstock.cpp.o.d
+++
@@ -1,2 +0,0 @@
-include/CMakeFiles/bbstock.dir/bbstock.cpp.o: \
-  /Users/joe/Documents/projects/c++_projects/student_marks_ica/include/bbstock.cpp
--- out/build/include/CMakeFiles/mmstock.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for mmstock.
-# This may be replaced when dependencies are built.
